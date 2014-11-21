744516ae19d37673f3d0b89273a2cebff01a035a
enkore <public@enkore.de>
2014-11-21 02:54:06 +0100

    dblist.py, clean.py

---
+++ clean.py
@@ -0,0 +1,17 @@
+
+import os
+import os.path
+import sys
+
+current_files = [os.path.basename(url) for url in sys.stdin]
+saved_bytes = 0
+
+for path in os.listdir(sys.argv[1]):
+    if (path not in current_files and
+        (path.endswith(".tar.xz") or path.endswith(".tar.xz.sig"))):
+        print("Removing old file '%s'" % path)
+        saved_bytes += os.stat(path).st_size
+        os.remove(path)
+
+if saved_bytes:
+    print("Reclaimed %d kilobytes disk space" % (saved_bytes / 1024))
---
+++ dblist.py
@@ -0,0 +1,24 @@
+
+import tarfile
+import urllib.request
+import sys
+import io
+
+REPO_NAME = sys.argv[1]
+REPO_URL = sys.argv[2] + "/" + sys.argv[3]
+REPO_ARCH = sys.argv[3]
+DB_URL = "%s/%s.db" % (REPO_URL, REPO_NAME)
+
+db_response = urllib.request.urlopen(DB_URL)
+
+db_response = io.BytesIO(db_response.readall())
+
+packages = []
+
+with tarfile.open(fileobj=db_response) as db:
+    packages = [name for name in db.getnames() if "/" not in name]
+
+print(DB_URL)
+for package in packages:
+    print("%s/%s-%s.pkg.tar.xz" % (REPO_URL, package, REPO_ARCH));
+    print("%s/%s-%s.pkg.tar.xz.sig" % (REPO_URL, package, REPO_ARCH));
