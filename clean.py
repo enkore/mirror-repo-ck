5da0833dd3a5dda5245e18f8959277e76e6883ec
enkore <public@enkore.de>
2014-11-21 10:44:25 +0100

    Fixed clean.py

--- clean.py
+++ clean.py
@@ -7,9 +7,10 @@ current_files = [os.path.basename(url) for url in sys.stdin]
 saved_bytes = 0
 
 for path in os.listdir(sys.argv[1]):
+    path = os.path.join(sys.argv[1], path)
     if (path not in current_files and
         (path.endswith(".tar.xz") or path.endswith(".tar.xz.sig"))):
-        print("Removing old file '%s'" % path)
+        print("Removing old file '%s'" % os.path.basename(path))
         saved_bytes += os.stat(path).st_size
         os.remove(path)
 
