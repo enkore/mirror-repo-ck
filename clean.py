230e825b1c403771fe5f38344e23191e5e4dd7f9
enkore <public@enkore.de>
2014-11-21 10:52:28 +0100

    Sometimes only printf-debugging can save the day.

--- clean.py
+++ clean.py
@@ -3,13 +3,13 @@ import os
 import os.path
 import sys
 
-current_files = [os.path.basename(url) for url in sys.stdin]
+current_files = [os.path.basename(url).strip() for url in sys.stdin]
 saved_bytes = 0
 
 for filename in os.listdir(sys.argv[1]):
     path = os.path.join(sys.argv[1], filename)
     if ((path.endswith(".tar.xz") or path.endswith(".tar.xz.sig")) and
-        filename.split(".tar.xz")[0] not in current_files):
+        filename not in current_files):
         print("Removing old file '%s'" % filename)
         saved_bytes += os.stat(path).st_size
         os.remove(path)
